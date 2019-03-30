bce07d422fd073ed98c9d3291a4aa60c1f7a7a4f
xSketchy0 <scoobyjai@hotmail.com>
2019-03-30 16:18:31 +0100

    Addedd registerHandler() for RoosterController

--- src/controller/Application.java
+++ src/controller/Application.java
@@ -12,20 +12,20 @@ public class Application {
 	 * een bepaalde poort (bijv. 8888). Daarna is er een PrIS-object gemaakt. Dit
 	 * object fungeert als toegangspunt van het domeinmodel. Hiervandaan kan alle
 	 * informatie bereikt worden.
-	 * 
+	 *
 	 * Om het domeinmodel en de Polymer-GUI aan elkaar te koppelen zijn diverse
 	 * controllers gemaakt. Er zijn meerdere controllers om het overzichtelijk te
 	 * houden. Je mag zoveel controller-klassen maken als je nodig denkt te hebben.
 	 * Elke controller krijgt een koppeling naar het PrIS-object om benodigde
 	 * informatie op te halen.
-	 * 
+	 *
 	 * Je moet wel elke URL die vanaf Polymer aangeroepen kan worden registreren!
 	 * Dat is hieronder gedaan voor een drietal URLs. Je geeft daarbij ook aan welke
 	 * controller de URL moet afhandelen.
-	 * 
+	 *
 	 * Als je alle URLs hebt geregistreerd kun je de server starten en de applicatie
 	 * in de browser opvragen! Zie ook de controller-klassen voor een voorbeeld!
-	 * 
+	 *
 	 */
 	public static void main(String[] args) {
 		JSONFileServer server = new JSONFileServer(new File("webapp/app"), 8888);
@@ -35,6 +35,7 @@ public class Application {
 		SysteemDatumController systeemDatumController = new SysteemDatumController(infoSysteem);
 		LoginController loginController = new LoginController(infoSysteem);
 		MedestudentenController medestudentenController = new MedestudentenController(infoSysteem);
+		RoosterController roosterController = new RoosterController(infoSysteem);
 
 		server.registerHandler("/systeemdatum/lesinfo", systeemDatumController);
 
@@ -42,9 +43,8 @@ public class Application {
 
 		server.registerHandler("/student/medestudenten/ophalen", medestudentenController);
 		server.registerHandler("/student/medestudenten/opslaan", medestudentenController);
+		server.registerHandler("/student/rooster/ophalen", roosterController);
 
 		server.start();
-
-		System.out.println(infoSysteem.getRooster("TICT-SIE-V1B"));
 	}
 }
